84dd5d43e4eb70e3037d47eef7b59905373615a6
Cal-Fang <59776160+Cal-Fang@users.noreply.github.com>
2020-12-04 19:09:57 -0600

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,2 +1,8 @@
 import pandas as pd
-pd.read_json (r'Path where you saved the JSON file\File Name.json')
+import os
+from pathlib import Path
+
+dead_voted = pd.read_json(Path(os.getcwd())/'out/dead_voters_who_voted.json', orient=str)
+dead_registered = pd.read_json(Path(os.getcwd())/'out/registered_dead_voters.json', orient=str)
+
+registered = pd.read_csv(Path(os.getcwd())/'mi_wa_voterfile.csv')
