a71664a3f44d7f45dd50eb949a3a8b2619ff32bc
Cal-Fang <59776160+Cal-Fang@users.noreply.github.com>
2020-12-05 18:49:27 -0600

    Update analysis.py
    Add one more dataset.

--- analysis.py
+++ analysis.py
@@ -5,4 +5,7 @@ from pathlib import Path
 dead_voted = pd.read_json(Path(os.getcwd())/'out/dead_voters_who_voted.json', orient=str)
 dead_registered = pd.read_json(Path(os.getcwd())/'out/registered_dead_voters.json', orient=str)
 
-registered = pd.read_csv(Path(os.getcwd())/'mi_wa_voterfile.csv')
+voted = pd.read_csv(Path(os.getcwd())/'mi_wa_voterfile.csv')
+detroit_zip = pd.read_csv(Path(os.getcwd())/'detroit_ziprange.csv')
+
+head(dead_voted)
