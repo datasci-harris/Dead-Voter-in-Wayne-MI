c4760de372fb9b17be9cf4e9c4cfbc3fcb82ba7f
Cal-Fang <59776160+Cal-Fang@users.noreply.github.com>
2020-12-06 23:53:28 -0600

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,9 +1,12 @@
+# Please run utility.py at first
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import utility                  # Self-created
 import geopandas as gpd
+import statsmodels.api as sm    # import statsmodels
 
 from pathlib import Path
 from scipy.stats import ttest_ind
@@ -12,6 +15,8 @@ over100_voted_MI = pd.read_json(Path(os.getcwd())/'out/dead_voters_who_voted.jso
 over100_registered_MI = pd.read_json(Path(os.getcwd())/'out/registered_dead_voters.json', orient=str)
 registered_incomplete_wayne = pd.read_csv(Path(os.getcwd())/'mi_wa_voterfile.csv')
 
+wayne_zip = registered_incomplete_wayne['zip_code'].unique().tolist()
+over100_wayne = over100_registered_MI.loc[over100_registered_MI['ZipCode'].isin(wayne_zip)]
 
 ## Plotting
 # Make a function to show the simple distribution
@@ -39,11 +44,12 @@ def simple_distri_plot(dataset, variable, xlab, title, categorical=True):
 simple_distri_plot(registered_incomplete_wayne, 'race_code', 'Race', 'Registered Voters by Race in Wayne County (Incomplete Data)')
 
 # Distribution of registered voters over 100 by race in Wayne County
-simple_distri_plot(registered_incomplete_wayne.loc[registered_incomplete_wayne["birth_year"]<=1920], 
+simple_distri_plot(registered_incomplete_wayne.loc[registered_incomplete_wayne["birth_year"]<=1920],
                    'race_code', 'Race', 'Registered Voters Over 100 by Race in Wayne County (Incomplete Data)')
 
 # Distribution of registered voters over 100 by birth_year in Michigan
-simple_distri_plot(over100_registered_MI, 'BirthYear', 'Birth Year', 'Registered Voters over 100 by Birth Year in Michigan', False)
+simple_distri_plot(over100_registered_MI, 'BirthYear', 'Birth Year', 'Registered Voters Over 100 by Birth Year in Michigan', False)
+
 
 ## Testing
 # Make a t.test function
@@ -58,15 +64,40 @@ def checkthebalance(dataset, groupby, variable, alpha):
         print('The difference between these two groups is insignficant at {} level.'.format(alpha))
 
 
-# Test the difference of age distribution between detroit and non-detroit area
+# Test the difference of over-100 vote stuats distribution between detroit and non-detroit area
 detroit = pd.read_csv(Path(os.getcwd())/'detroit_ziprange.csv')
-rename = {'first_name':'FirstName', 'last_name':'LastName', 'birth_year':'BirthYear', 'zip_code':'ZipCode'}
-whole = pd.concat([over100_registered_MI, registered_incomplete_wayne.rename(columns=rename)], join='inner')
-whole
-birthyeartoage(whole, 'BirthYear', 2020)
-ziptorange(whole, detroit['zip'], 'ZipCode', 'detroit')
-checkthebalance(whole, 'detroit', 'Age', 0.05)
-
-# Test the difference of vote stuats distribution between detroit and non-detroit area
 ziptorange(over100_registered_MI, detroit['zip'], 'ZipCode', 'detroit')
 checkthebalance(over100_registered_MI, 'detroit', 'Voted', 0.05)
+
+# Test the difference of age distribution between detroit and non-detroit area in Wayne County
+rename = {'first_name':'FirstName', 'last_name':'LastName', 'birth_year':'BirthYear', 'zip_code':'ZipCode'}
+wayne = pd.concat([over100_wayne, registered_incomplete_wayne.rename(columns=rename)], join='outer')
+
+birthyeartoage(wayne, 'BirthYear', 2020)
+ziptorange(wayne, detroit['zip'], 'ZipCode', 'detroit')
+checkthebalance(wayne, 'detroit', 'Age', 0.05)
+
+
+# Regression analysis
+## Rename and create variable
+over100(wayne, 'BirthYear', 2020)
+wayne = organize_byzip(wayne, 'Over100', 'ZipCode')
+wayne['Over100_per'] = 100*wayne[1]/(wayne[1]+wayne[0])
+
+## Merge in gross income dataset
+income = pd.read_csv(Path(os.getcwd())/'income by zipcode.csv').dropna()
+income['ZipCode'] = income['zipcode'].astype(int)
+ziptorange(income, detroit['zip'], 'ZipCode', 'Detroit')
+
+df = pd.merge(wayne, income, on=['ZipCode'])
+df['income'] = np.log2(df['income'])
+
+## Regression
+X = df[['Over100_per','income']]
+y = df['Detroit']
+X = sm.add_constant(X) ## let's add an intercept (beta_0) to our model
+model = sm.OLS(y, X).fit()
+predictions = model.predict(X)
+
+## Print out the statistics
+model.summary()
