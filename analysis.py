e51715a43fe75dcf03d1f94eb8a11635bd521bcd
ziyiwang98 <62902751+ziyiwang98@users.noreply.github.com>
2020-12-07 11:56:37 +0800

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import utility                  # Self-created
+import geopandas as gpd
 
 from pathlib import Path
 from scipy.stats import ttest_ind
 
-dead_voted = pd.read_json(Path(os.getcwd())/'out/dead_voters_who_voted.json', orient=str)
-dead_registered = pd.read_json(Path(os.getcwd())/'out/registered_dead_voters.json', orient=str)
-registered = pd.read_csv(Path(os.getcwd())/'mi_wa_voterfile.csv')
+over100_voted_MI = pd.read_json(Path(os.getcwd())/'out/dead_voters_who_voted.json', orient=str)
+over100_registered_MI = pd.read_json(Path(os.getcwd())/'out/registered_dead_voters.json', orient=str)
+registered_incomplete_wayne = pd.read_csv(Path(os.getcwd())/'mi_wa_voterfile.csv')
 
 
 ## Plotting
@@ -34,15 +35,15 @@ def simple_distri_plot(dataset, variable, xlab, title, categorical=True):
     plt.close()
     return
 
-# Distribution of all registered voters by race
-simple_distri_plot(registered, 'race_code', 'Race', 'Registered Voters by Race in Wayne County (Incomplete Data)')
+# Distribution of all registered voters by race in Wayne County
+simple_distri_plot(registered_incomplete_wayne, 'race_code', 'Race', 'Registered Voters by Race in Wayne County (Incomplete Data)')
 
-# Distribution of registered voters over 100 by race in the registered voter dataset
-simple_distri_plot(registered.loc[registered["birth_year"]<=1920], 'race_code', 'Race', 'Registered Voters Over 100 by Race in Wayne County (Incomplete Data)')
+# Distribution of registered voters over 100 by race in Wayne County
+simple_distri_plot(registered_incomplete_wayne.loc[registered_incomplete_wayne["birth_year"]<=1920], 
+                   'race_code', 'Race', 'Registered Voters Over 100 by Race in Wayne County (Incomplete Data)')
 
-# Distribution of all dead registered voters by birth_year
-simple_distri_plot(dead_registered, 'BirthYear', 'Birth Year', 'Dead Voters by Birth Year in Wayne County', False)
-
+# Distribution of registered voters over 100 by birth_year in Michigan
+simple_distri_plot(over100_registered_MI, 'BirthYear', 'Birth Year', 'Registered Voters over 100 by Birth Year in Michigan', False)
 
 ## Testing
 # Make a t.test function
@@ -60,11 +61,12 @@ def checkthebalance(dataset, groupby, variable, alpha):
 # Test the difference of age distribution between detroit and non-detroit area
 detroit = pd.read_csv(Path(os.getcwd())/'detroit_ziprange.csv')
 rename = {'first_name':'FirstName', 'last_name':'LastName', 'birth_year':'BirthYear', 'zip_code':'ZipCode'}
-whole = pd.concat([dead_registered, registered.rename(columns=rename)], join='inner')
+whole = pd.concat([over100_registered_MI, registered_incomplete_wayne.rename(columns=rename)], join='inner')
+whole
 birthyeartoage(whole, 'BirthYear', 2020)
 ziptorange(whole, detroit['zip'], 'ZipCode', 'detroit')
 checkthebalance(whole, 'detroit', 'Age', 0.05)
 
 # Test the difference of vote stuats distribution between detroit and non-detroit area
-ziptorange(dead_registered, detroit['zip'], 'ZipCode', 'detroit')
-checkthebalance(dead_registered, 'detroit', 'Voted', 0.05)
+ziptorange(over100_registered_MI, detroit['zip'], 'ZipCode', 'detroit')
+checkthebalance(over100_registered_MI, 'detroit', 'Voted', 0.05)
